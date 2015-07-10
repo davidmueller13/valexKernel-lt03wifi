5761c4266501d9a814352f5301afa678441f3e95
Vidir Alexander Jonsson <valex@valex.is>
2015-07-10 10:16:41 +0000

    Revert "arm: arch_timer: add isbs to register accessors"
    This reverts commit 7459c0ba603149bb9057125918299a7487b751c3.

--- arch/arm/include/asm/arch_timer.h
+++ arch/arm/include/asm/arch_timer.h
@@ -55,8 +55,6 @@ static inline void arch_timer_reg_write(const int access, const int reg, u32 val
 			break;
 		}
 	}
-
-	isb();
 }
 
 static inline u32 arch_timer_reg_read(const int access, const int reg)
@@ -99,7 +97,6 @@ static inline u64 arch_counter_get_cntpct(void)
 {
 	u64 cval;
 
-	isb();
 	asm volatile("mrrc p15, 0, %Q0, %R0, c14" : "=r" (cval));
 	return cval;
 }
@@ -108,7 +105,6 @@ static inline u64 arch_counter_get_cntvct(void)
 {
 	u64 cval;
 
-	isb();
 	asm volatile("mrrc p15, 1, %Q0, %R0, c14" : "=r" (cval));
 	return cval;
 }
