4c73add4d6b6af0f56912236f88291f8befdf17c
Vidir Alexander Jonsson <valex@valex.is>
2015-07-12 03:20:01 +0000

    arm/smp: fix build error

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -87,14 +87,8 @@ int __cpu_up(unsigned int cpu, struct task_struct *idle)
 	 * its stack and the page tables.
 	 */
 	secondary_data.stack = task_stack_page(idle) + THREAD_START_SP;
-#ifdef CONFIG_ARM_MPU
-	secondary_data.mpu_rgn_szr = mpu_rgn_info.rgns[MPU_RAM_REGION].drsr;
-#endif
-
-#ifdef CONFIG_MMU
-	secondary_data.pgdir = get_arch_pgd(idmap_pgd);
-	secondary_data.swapper_pg_dir = get_arch_pgd(swapper_pg_dir);
-#endif
+	secondary_data.pgdir = virt_to_phys(idmap_pgd);
+	secondary_data.swapper_pg_dir = virt_to_phys(swapper_pg_dir);
 	sync_cache_w(&secondary_data);
 
 	/*
