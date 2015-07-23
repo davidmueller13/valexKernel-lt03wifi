7f75d68406a7c1041a062947e951701777cf10f0
Vidir Alexander Jonsson <valex@valex.is>
2015-07-23 00:32:55 +0000

    Revert "ARM: SMP: add function arch_send_wakeup_ipi_mask()"
    This reverts commit 4804a28fd36d5ec12e0dda9bf3b8f097002c2fc7.

--- arch/arm/include/asm/smp.h
+++ arch/arm/include/asm/smp.h
@@ -84,7 +84,6 @@ extern void arm_send_ping_ipi(int cpu);
 
 extern void arch_send_call_function_single_ipi(int cpu);
 extern void arch_send_call_function_ipi_mask(const struct cpumask *mask);
-extern void arch_send_wakeup_ipi_mask(const struct cpumask *mask);
 
 extern int register_ipi_completion(struct completion *completion, int cpu);
 
