f16992bdc4fac5f6e61ae67be0a04984f44d4c07
Vidir Alexander Jonsson <valex@valex.is>
2015-07-30 17:13:25 +0000

    ARM: b.L switcher: adapt to gic changes after merge

--- arch/arm/common/bL_switcher.c
+++ arch/arm/common/bL_switcher.c
@@ -33,6 +33,7 @@
 #include <linux/sysfs.h>
 #include <linux/moduleparam.h>
 #include <linux/of.h>
+#include <linux/irqchip/arm-gic.h>
 
 #include <asm/smp_plat.h>
 #include <asm/cacheflush.h>
@@ -40,7 +41,6 @@
 #include <asm/suspend.h>
 #include <asm/mcpm.h>
 #include <asm/bL_switcher.h>
-#include <asm/hardware/gic.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/power_cpu_migrate.h>
