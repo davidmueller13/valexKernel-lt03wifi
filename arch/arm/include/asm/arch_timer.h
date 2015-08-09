22e0b23d6d24c87e9ddce70bd1da6cc99b36d2cb
Vidir Alexander Jonsson <valex@valex.is>
2015-08-09 20:48:55 +0000

    Revert "ARM: architected timers: add DT support"
    This reverts commit f8729dc9921cbd16c16bde42cea5c962f31a97c4.

--- arch/arm/include/asm/arch_timer.h
+++ arch/arm/include/asm/arch_timer.h
@@ -10,14 +10,8 @@ struct arch_timer {
 #ifdef CONFIG_ARM_ARCH_TIMER
 int arch_timer_register(struct arch_timer *);
 int arch_timer_sched_clock_init(void);
-int arch_timer_of_register(void);
 #else
 static inline int arch_timer_register(struct arch_timer *at)
-{
-	return -ENXIO;
-}
-
-static inline int arch_timer_of_register(void)
 {
 	return -ENXIO;
 }
