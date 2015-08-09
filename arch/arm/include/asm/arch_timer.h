3f27da8c48d7b7e505a19e8270bffc814b1eea9d
Marc Zyngier <marc.zyngier@arm.com>
2015-08-09 22:01:29 +0000

    ARM: 7522/1: arch_timers: register a time/cycle counter
    Some subsystems (KVM for example) need access to a cycle counter.
    In the KVM case, this is used to measure the time delta between
    host and guest in order to accurately generate timer events for
    the guest.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/arch_timer.h
+++ arch/arm/include/asm/arch_timer.h
@@ -4,6 +4,7 @@
 #include <linux/ioport.h>
 
 #include <asm/errno.h>
+#include <linux/clocksource.h>
 
 struct arch_timer {
 	struct resource	res[2];
@@ -13,6 +14,7 @@ struct arch_timer {
 #define ARCH_HAS_READ_CURRENT_TIMER
 int arch_timer_register(struct arch_timer *);
 int arch_timer_sched_clock_init(void);
+struct timecounter *arch_timer_get_timecounter(void);
 #else
 static inline int arch_timer_register(struct arch_timer *at)
 {
@@ -22,6 +24,11 @@ static inline int arch_timer_register(struct arch_timer *at)
 static inline int arch_timer_sched_clock_init(void)
 {
 	return -ENXIO;
+}
+
+static inline struct timecounter *arch_timer_get_timecounter(void)
+{
+	return NULL;
 }
 #endif
 
